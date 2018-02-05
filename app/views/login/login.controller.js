946c917c442cf973be187907f897191c428b498f
Ivaylo Banchevski <ivaylo@dagroup.bg>
2018-02-05 21:49:24 +0200

    Update login page
    Add "Animate.css" library to make the page more dynamic. Font awesome is
    added as well. The login page seems to load different states but it just
    creates / destroys templates, which adds a cool look.

--- app/views/login/login.controller.js
+++ app/views/login/login.controller.js
@@ -1,5 +1,10 @@
 function LoginCtrl() {
+    var vm = this;
+    vm.render = 'enter';
 
+    vm.setRender = function(render) {
+        vm.render = render;
+    };
 }
 LoginCtrl.$inject = [];
 angular.module('letterSoup').controller('LoginCtrl', LoginCtrl);
