ec8bf93bbc5ee85f7e2103828a17a34586aca5fa
Ivaylo Banchevski <ivaylo@dagroup.bg>
2018-02-05 22:58:42 +0200

    Update registration page design
    Add front-end field validation. Additional fields (city and school) are
    added. Available subjects would be pulled from the database and selected
    in the form instead of just typed in a field.

--- app/views/register/register.controller.js
+++ app/views/register/register.controller.js
@@ -1,5 +1,28 @@
 function RegisterCtrl() {
+    var vm = this;
+    vm.repeatedPassword = '';
+    vm.subjects = [];
+    vm.teacher = {
+        "firstName": '',
+        "lastName": '',
+        "city": '',
+        "school": '',
+        "email": '',
+        "password": '',
+        "subject": ''
+    };
 
+    var mockSubjects = [
+        {
+            "name": 'Български',
+            "_id": '23xcvadf'
+        },
+        {
+            "name": 'Испански',
+            "_id": '23x324vadf'
+        }
+    ];
+    vm.subjects = mockSubjects;
 }
 RegisterCtrl.$inject = [];
 angular.module('letterSoup').controller('RegisterCtrl', RegisterCtrl);
